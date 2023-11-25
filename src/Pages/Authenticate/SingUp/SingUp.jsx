f902d847f4cc6cdcf32e203cc57fc7bebcd74038
Alamin-22 <mdalaminmollick@gmail.com>
2023-11-25 13:45:10 +0600

    updated singup page

--- src/Pages/Authenticate/SingUp/SingUp.jsx
+++ src/Pages/Authenticate/SingUp/SingUp.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useLoaderData } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import Header from "../../../Components/HeaderFooter/Header/Header";
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [Upzila, setUpzila] = useState([]);
+    const status = "Active";
     const Districts = useLoaderData()
     const handleSignUp = (e) => {
         e.preventDefault();
@@ -13,8 +15,16 @@ const SignUp = () => {
         const email = form.get("email");
         const password = form.get("password");
         const ConfirmPassword = form.get("ConfirmPassword");
-        const name = form.get("name");
+        const FirstName = form.get("FirstName");
+        const LastName = form.get("LastName");
+        const Name = FirstName + " " + LastName
         const photo = form.get("photo");
+        const District = form.get("District");
+        const Upazila = form.get("Upazila");
+        const bloodGroup = form.get("bloodGroup");
+        const Status = form.get("Status");
+
+
         // confirm password validation
 
         if (password === ConfirmPassword) {
@@ -32,7 +42,8 @@ const SignUp = () => {
 
 
 
-        console.log(email, name, photo);
+        const info = { Name, email, District, Upazila, bloodGroup, Status, photo }
+        console.log(info);
         // Rest of your code for user creation and profile update
         // Uncomment and complete the remaining functionality based on your requirements
 
@@ -66,7 +77,8 @@ const SignUp = () => {
 
     return (
         <div>
-            <div>
+            <Header></Header>
+            <div className=" hero flex justify-center items-center min-h-[95vh] " style={{ backgroundImage: 'url(https://i.ibb.co/JcsjxJP/bannere3.jpg)' }}>
                 <div className="w-full lg:w-2/3 mx-auto mt-6 mb-9">
                     <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
                         <form onSubmit={handleSignUp} className="card-body bg-emerald-50 shadow-2xl rounded-xl ">
@@ -76,13 +88,13 @@ const SignUp = () => {
                                     <label className="label">
                                         <span className="label-text">First Name</span>
                                     </label>
-                                    <input type="text" name="FirstName" placeholder="First Name" className="input input-bordered" required />
+                                    <input type="text" name="FirstName" placeholder="First Name" className="input input-bordered input-accent" required />
                                 </div>
                                 <div className="form-control w-1/2">
                                     <label className="label">
                                         <span className="label-text">Last Name</span>
                                     </label>
-                                    <input type="text" name="LastName" placeholder="Last Name" className="input input-bordered" required />
+                                    <input type="text" name="LastName" placeholder="Last Name" className="input input-bordered input-accent" required />
                                 </div>
                             </div>
 
@@ -90,7 +102,7 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name="email" placeholder="example@gmail.com" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="example@gmail.com" className="input input-bordered input-accent" required />
                             </div>
                             {/* password row */}
                             <div className="form-control relative">
@@ -106,7 +118,7 @@ const SignUp = () => {
                                                 type={showPassword ? "text" : "password"}
                                                 name="password"
                                                 placeholder="password"
-                                                className="input input-bordered w-full"
+                                                className="input input-bordered input-accent w-full"
                                                 required
                                             />
                                             <span className="absolute right-5 bottom-2 cursor-pointer" onClick={() => { setShowPassword(!showPassword) }} >
@@ -123,7 +135,7 @@ const SignUp = () => {
                                                 type={showPassword ? "text" : "password"}
                                                 name="ConfirmPassword"
                                                 placeholder="Confirm password"
-                                                className="input input-bordered w-full"
+                                                className="input input-bordered input-accent w-full"
                                                 required
                                             />
                                             <span className="absolute right-5 bottom-2 cursor-pointer" onClick={() => { setShowPassword(!showPassword) }} >
@@ -144,7 +156,7 @@ const SignUp = () => {
                                             <label className="label">
                                                 <span className="label-text">District</span>
                                             </label>
-                                            <select name="bloodGroup" className="select select-accent w-full max-w-sm" defaultValue="" required>
+                                            <select name="District" className="select select-accent w-full max-w-sm" defaultValue="" required>
                                                 <option value="" disabled>Select District</option>
                                                 {Districts.map((district) => (
                                                     <option key={district.id} value={district.name}>
@@ -157,7 +169,7 @@ const SignUp = () => {
                                             <label className="label">
                                                 <span className="label-text">Upazila</span>
                                             </label>
-                                            <select name="bloodGroup" className="select select-accent w-full max-w-sm" defaultValue="" required>
+                                            <select name="Upazila" className="select select-accent w-full max-w-sm" defaultValue="" required>
                                                 <option value="" disabled>Select Upazila</option>
                                                 {Upzila.map((upzila) => (
                                                     <option key={upzila.id} value={upzila.name}>
@@ -186,8 +198,8 @@ const SignUp = () => {
                                 </div>
 
                             </div>
-                            {/* img and blood group */}
-                            <div className="">
+                            {/* img and status  */}
+                            <div className="flex gap-3 justify-between">
                                 <div className="form-control">
                                     <div className="flex items-center justify-between">
                                         <div>
@@ -198,6 +210,13 @@ const SignUp = () => {
                                         </div>
                                     </div>
                                 </div>
+                                <div>
+                                    <label className="label">
+                                        <span className="label-text">Status</span>
+                                    </label>
+                                    <input type="text" readOnly name="Status" defaultValue={status} className="input  input-bordered input-accent" required />
+                                </div>
+
                             </div>
 
                             <div className="form-control mt-6">
