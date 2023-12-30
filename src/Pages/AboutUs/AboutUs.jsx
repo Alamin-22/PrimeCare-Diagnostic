6e6ad5fb63cef0b9178786f66169f4dbe8a208ad
Alamin-22 <mdalaminmollick@gmail.com>
2023-12-31 01:57:16 +0600

    added subscription section

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,6 +1,7 @@
 import Docs from "./AboutComponents/Docs";
 import Gallery from "./AboutComponents/Gallery";
 import Intro from "./AboutComponents/Intro";
+import Pricing from "./AboutComponents/Pricing";
 
 const AboutUs = () => {
     return (
@@ -8,6 +9,7 @@ const AboutUs = () => {
             <Intro></Intro>
             <Gallery></Gallery>
             <Docs></Docs>
+            <Pricing></Pricing>
         </div>
     );
 };
